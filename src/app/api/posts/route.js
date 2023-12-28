56b91d55427d39b987ff5c87c72e1670154d12a7
Elri <87elrivdm@gmail.com>
2023-12-28 22:17:15 +0200

    Added Post route, get, post & delete api

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -5,38 +5,25 @@ import { NextResponse } from "next/server.js";
 
 //POST - api/posts
 
-//GET Title
-export async function GET() {
+/*GET Title
+export default async function GET() {
   const title = await prisma.post.findMany({
     orderBy: {
       createdAt: "desc",
     },
   });
   return NextResponse.json({ success: true, title });
-}
-
-//POST title
-export async function POST(request, response) {
-  try {
-    const { title } = await request.json();
-    if (!title) {
-      return NextResponse.json({
-        success: false,
-        error: "You must provide a title",
-      });
-    }
-    const findTitle = await prisma.post.findFirst({ where: { title } });
-
-    /*if(findTitle) {
-return NextResponse.json({
-    success: false,
-    error: "Must have a unique title"
-})
 }*/
-    const newTitle = await prisma.post.create({
-      data: { title },
-    });
-    return NextResponse.json({ success: true, title });
+
+//POST
+export default async function postHandler(req, res) {
+  try {
+    if (req.method === "POST") {
+      const posts = await prisma.post.findMany();
+      res.json(posts);
+    } else {
+    }
+    return NextResponse.json({ success: true, message: "Post is successful" });
   } catch (error) {
     return NextResponse.json({ success: false, error: error.message });
   }
