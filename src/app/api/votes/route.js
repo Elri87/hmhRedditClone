aa1cdf5d002bd233d98098ef25849cb870999b85
Elri <87elrivdm@gmail.com>
2023-12-28 22:02:32 +0200

    added vote route.js

---
+++ src/app/api/votes/route.js
@@ -0,0 +1,20 @@
+import { prisma } from "@/lib/prisma.js";
+
+export default async function handleVote(postId, voteType) {
+  try {
+    const vote = await prisma.vote.create({
+      data: {
+        postId,
+        voteType,
+      },
+    });
+
+    if (vote) {
+      return { success: true, message: "Vote successful" };
+    } else {
+      return { success: false, error: "Failed to create vote" };
+    }
+  } catch (error) {
+    return { success: false, error: error.message };
+  }
+}
