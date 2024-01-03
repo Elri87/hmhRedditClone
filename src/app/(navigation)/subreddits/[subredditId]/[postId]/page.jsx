f1c3553a974b409dd5178a88a8a23faf86999087
Elri <87elrivdm@gmail.com>
2024-01-04 00:36:10 +0200

    edited page.jsx for [postId]

---
+++ src/app/(navigation)/subreddits/[subredditId]/[postId]/page.jsx
@@ -0,0 +1,51 @@
+import { prisma } from "@/lib/prisma.js";
+import { fetchUser } from "@/lib/fetchUser.js";
+
+import CreateComment from "@/components/CreateComment.jsx";
+
+export default async function PostAndComments({ params }) {
+  const { subredditId, postId } = params;
+
+  const user = await fetchUser();
+
+  const votes = await prisma.vote.findMany();
+
+  //find post woth specific id
+  const mainPost = await prisma.post.findFirst({
+    where: {
+      id: postId,
+      parentId: null,
+    },
+    include: {
+      user: true,
+      subreddit: true,
+      children: {
+        include: {
+          user: true,
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      },
+    },
+  });
+  delete mainPost.user.password;
+
+  let checkUser;
+
+  if (user.id) {
+    checkUser = await prisma.vote.findFirst({
+      where: {
+        postId: postId,
+        userId: user.id,
+      },
+    });
+  }
+  return (
+    <section>
+      <h1>r/ {mainPost.subreddit.name}</h1>
+
+      <CreateComment user={user} subredditId={subredditId} post={mainPost} />
+    </section>
+  );
+}
